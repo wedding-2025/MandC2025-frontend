eca6c446e16507bc2fdaa448851abefb7eb5c8ff
Dubem-Umeh-Raphael <raphaelumeh21@gmail.com>
2025-03-20 16:24:34 +0000

    removed removed visibility from the upload button

--- src/wed-components/RECAP/New/Recap-1.1.jsx
+++ src/wed-components/RECAP/New/Recap-1.1.jsx
@@ -190,7 +190,9 @@ const RecapWrapper = () => {
 
   // Optimized Images from Cloudinary
   const optimizedImage = (url) => {
-    return url.replace("/upload/", "/upload/f_webp,q_auto,w_auto/");
+    return url
+      .replace("/upload/", "/upload/f_webp,q_auto,w_auto/")
+      .replace(/\.(png|jpe?g|gif)/i, ".webp");
   };  
 
 
@@ -214,7 +216,7 @@ const RecapWrapper = () => {
       }}
     >
       <div className="p-6 flex flex-col mx-auto items-center mt-0 bg-white/40 backdrop-blur-lg !z-20 mb-0 h-full w-full" style={{ opacity: '1', height: '100%' }}>
-        <div className="flex flex-row-reverse items-center justify-center space-x-6 mb-5 mx-auto">{/* Remove hidden to add flex (vise-versa) */}
+        <div className="hidden flex-row-reverse items-center justify-center space-x-6 mb-5 mx-auto">{/* Remove hidden to add flex (vise-versa) */}
           <input
             type="file"
             id="file-upload"

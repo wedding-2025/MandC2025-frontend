c3caf1d84d401177848c46ce5bff6e738d7b8ceb
Dubem Umeh <raphaelumeh21@gmail.com>
2025-06-20 16:53:30 +0000

    Update RsvpPage.jsx

--- src/pages/RsvpPage.jsx
+++ src/pages/RsvpPage.jsx
@@ -15,11 +15,11 @@ const RsvpPage = ({ scrollToContact }) => {
         
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="RSVP - M & C 2025" />
-        <meta property="og:description" content="Let us know if you'll be attending Charles and Maryjane’s wedding in 2025. RSVP now and join the celebration!" />
+{/*         <meta property="og:description" content="Let us know if you'll be attending Charles and Maryjane’s wedding in 2025. RSVP now and join the celebration!" />
         <meta property="og:image" content="https://res.cloudinary.com/dzsuia2ia/image/upload/w_1000,ar_3:2,c_fill,g_auto,/v1733837836/aggwqocfwgsjfvannbxe.jpg" />
         <meta property="og:url" content="https://mandc2025.org/rsvp/rsvp-form" />
         <meta property="og:type" content="website" />
-        <link rel="preload" type="image/png" href="https://res.cloudinary.com/dzsuia2ia/image/upload/v1733482107/ziuecv6cwccb7oj1nkba.png" as="image" />
+        <link rel="preload" type="image/png" href="https://res.cloudinary.com/dzsuia2ia/image/upload/v1733482107/ziuecv6cwccb7oj1nkba.png" as="image" /> */}
       </Helmet>
 
 
